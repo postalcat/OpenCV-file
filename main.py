6e3a20d667edaab46c806a292dc5c66270ebd89d
Oliver Baldwin <oliver.baldwin@xavier.vic.edu.au>
2023-10-10 12:21:22 +1100

    Base commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Creating an AI that can recoginize objects in a video.
+# Boxing simulator? Just setup main interface first
+import cv2 as cv
+import PIL as pil
+import tkinter as tk
+import ttkbootstrap as ttk
+
