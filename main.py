4fefe8197d7cc4e2f9a0e8ab078a5878049a1f07
Oliver Baldwin <oliver.baldwin@xavier.vic.edu.au>
2023-10-20 09:18:08 +1100

    preperation

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-#Creating an AI that can recoginize objects in a video.
-# Boxing simulator? Just setup main interface first
-import cv2 as cv
-import PIL as pil
-import tkinter as tk
-import ttkbootstrap as ttk
-
+#Recognizing repetitive symbols within a given image.
